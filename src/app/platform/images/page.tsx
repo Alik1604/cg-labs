64f1f7e9d1acef6866eb2b94dce5c5689b0e7e51
Andriy Panchiy <andrew@chromane.com>
2023-11-16 11:15:10 +0200

    small fix

--- src/app/platform/images/page.tsx
+++ src/app/platform/images/page.tsx
@@ -187,6 +187,17 @@ const Images: React.FC = () => {
     // }
   }, [selectedArea]);
 
+  const handleSave = () => {
+    const canvas = canvasRef1.current;
+    if (!canvas) return;
+
+    const imageUrl = canvas.toDataURL("image/png");
+    const a = document.createElement("a");
+    a.href = imageUrl;
+    a.download = "img.png";
+    a.click();
+  };
+
   return (
     <main className="flex min-h-screen flex-row bg-white">
       <Navigation />
@@ -247,6 +258,13 @@ const Images: React.FC = () => {
             }}
           >
             Clear selection
+          </button>
+          <button
+            onClick={handleSave}
+            type="button"
+            className="w-[100px] h-[40px] focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          >
+            Save
           </button>
         </div>
         <div className="flex justify-between my-4">
